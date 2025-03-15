68ec06be8616bc8b70761100275a8c40e7b8634e
Sanjeev N <worldsansgamming@gmail.com>
2025-03-16 00:57:00 +0530

    s

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from todo import models
 from todo.models import TODOO
+from django.contrib.auth import authenticate,login,logout
 
 def signup(request):
     if request.method == 'POST':
@@ -10,11 +11,22 @@ def signup(request):
         password = request.POST.get('password')
         print( name,  email, password )
 
-        my_user = User.objects.create_user(name,email,password)
+        my_user = User.objects.create_user(username=name, email=email, password=password)
         my_user.save()
         return redirect('/login')
 
     return render(request, 'signup.html')
 
-def login(request):
-    return render(request, 'signup.html')
+def loginn(request):
+     if request.method == 'POST':
+        name = request.POST.get('username')
+        Password = request.POST.get('password')
+        userr=authenticate(request,username=name, password=Password)
+        if userr is not None:
+            login(request,userr)
+            return redirect('/todopage')
+        else:
+            return redirect('/login')
+
+    
+     return render(request, 'login.html')
