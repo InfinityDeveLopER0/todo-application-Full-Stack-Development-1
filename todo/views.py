ca8a485a94958ab7d2725719c9dfe6abb51c5365
Sanjeev N <worldsansgamming@gmail.com>
2025-03-15 17:55:39 +0530

    c

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 def signup(request):
+    if request.method == 'POST':
+        name = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        print( name,  email, password )
+
     return render(request, 'signup.html')
